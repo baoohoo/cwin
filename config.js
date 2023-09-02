57fcc9d47ca768f2744500814ef8d37dc65a6fb1
Bảo Hồ <hophamgiabao4b@gmail.com>
2023-09-02 23:49:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+//注册链接
+const config_registerLinks = [
+    "https://www.99win55.com",
+    "https://www.99win55.com",
+    "https://www.99win55.com",
+    "https://www.99win55.com",
+    "https://www.99win55.com",
+    "https://www.99win55.com"
+]
+const config_phone1 ='131242202117'//手机号码1
+const config_phone2 = '17645485273'//手机号码2
+//客服地址
+const config_kefu_url = "https://isdkfe.csgyc6isnb.com/chatwindow.aspx?siteId=65000567&planId=af69d421-bf1b-4b89-80f9-4ea0ca7bb53a&_=1684214771078"
+//站点跳转地址
+const config_site_url = "https://www.99win55.com"
+const api = "https://110cwin.com"//服务器API地址
